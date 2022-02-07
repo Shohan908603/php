9c3de9cd671c4abfea8243cbb42d4046b87c5b5d
shohan <shohanurrahman2334@gmail.com>
2022-02-07 16:03:18 +0600

    php add

---
+++ e-commerce/index.php
@@ -0,0 +1,446 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/logo.png" type="image/icon">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce</title>
+</head>
+<body>
+
+    <!--top headerend-->
+
+    <div class="top-header">
+        <div class="top-header-1st">
+            <i class="fas fa-envelope"></i>
+            Shohanurrahman2334@gmail.com 
+            
+        </div>
+        
+        <div class="top-header-2nd">
+            <div class="top-header-phone">
+                    <i class="fas fa-phone-alt"></i>
+                    +88 01314441196
+           </div>
+           <div class="top-header-icon">
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-google-plus-g"></i>
+           </div>
+        </div>
+        <div class="top-header-3rd">
+            <img src="img/america2.png">
+            America
+        </div>
+        <div class="top-header-4th">
+            <i class="fas fa-user-circle"></i>
+             LogIn | Register 
+        </div>
+
+    </div>
+
+    <!--top header end-->
+
+    <!-- meddle headding start-->
+
+
+    <nav class="navbar">
+        <div class="nav">
+            <img src="img/logo.png" class="brand-logo" alt="">
+            <div class="nav-items">
+                <div class="search">
+                    <input type="text" class="search-box" placeholder="search fruits product">
+                    <button class="search-btn"><i class="fas fa-search"></i></button>
+                </div>
+                <a href=""><i class="far fa-heart"></i></a>
+                <a href=""><i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        </nav>
+
+
+ <!--meddle heading end-->       
+
+<!-- header navber start-->
+
+<ul class=" links-container">
+    <li class="link-item"><a href="#" class="link">Home</a></li>
+    <li class="link-item"><a href="loginfrom.html" class="link">LogIn</a></li>
+    <li class="link-item"><a href="#" class="link">Cheakout</a></li>
+    <li class="link-item"><a href="#" class="link">Registration</a></li>
+    <li class="link-item"><a href="#" class="link">Blog</a></li>
+    <li class="link-item"><a href="contact.html" class="link">Contact</a></li>
+</ul>
+
+
+
+<!-- header navber end-->
+
+<!--hero section start-->
+
+<header class="hero-section">
+    <div class="content">
+        <img src="img/headerbg.png" class="logo" alt="">
+        <p class="sub-heading">best fruits collection of all time</p>
+    </div>
+</header>
+
+
+<!--hero section end-->
+
+<!--banner overal start-->
+<div class="margin">
+
+<section class="banner-overal-section">
+    
+        <div class="banner-overal-container">
+            <div class="banner-overal-item1"></div>
+            <div class="banner-overal-item2"></div>
+        </div>
+    
+</section>
+
+<section class="about">
+
+    <div class="about-container">
+
+        <div class="about-container-1">
+            
+        </div>
+        <div class="about-container-2">
+
+            <h4> A Few Woards About Us</h4><br>
+
+            <h2>A shop for good People <br>by good People</h2><br>
+            <p>Decay is caused by bacteria that collect on teeth and feed on the arbohydrates
+            in our diet.The bacteria produce acid that wears away at the enamel on our teeth.
+            If decay is left untreated,it can cause pain infen.</p>
+
+        
+        
+            
+            <ul>
+                
+                <li>  Lorem ipsum dolor, sit amet consectetur adipisicing elit. </li> 
+                <li>  Lorem ipsum dolor, sit amet consectetur adipisicing elit. </li>
+                <li>  Lorem ipsum dolor, sit amet consectetur adipisicing elit. </li>
+                <li>  Lorem ipsum dolor, sit amet consectetur adipisicing elit. </li>
+                
+            </ul>
+            
+        </div>
+
+    </div>
+
+</section>
+
+<!--banner overal end-->
+
+<!--product section 1 start-->
+<br><br><br><br><hr style="color: rgb(3, 117, 28);height: 1px;"><br><br><br><br>
+
+<section class="trending-products">
+    <div class="trending-header">
+        <p>Fruits Store</p>
+        <h1>Our Fruits Product</h1>
+    </div>
+    <div class="products-body">
+        <div class="product">
+            <img src="img/card.PNG" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card1.PNG" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card2.PNG" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card3.PNG" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+    </div>
+    <div class="products-body">
+        <div class="product">
+            <img src="img/card4.png" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card5.png" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card6.png" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card1.png" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+    </div>
+    
+</section>
+
+</div>
+<!--product section 1 end-->
+
+<!--middle banner start-->
+
+
+<section class="middle-banner">
+
+    <div class="middle-text">
+        <h2>Our  Best Fruits product 2022</h2>
+        <h4>Fruits Best Products</h4>
+        <h6>This Nature health full product.</h6>
+        <div class="middle-btn">
+            <a href="#" class="middle-btn-btn">Shop Now</a>
+        </div>			
+    </div>
+    
+</section>
+
+<!--middle banner end -->
+
+
+
+
+
+
+<!--product section 2 start-->
+<div class="margin">
+
+<section class="trending-products-2">
+    <div class="trending-header">
+        <p>Fruits Store</p>
+        <h1>Our Fruits Product</h1>
+    </div>
+    <div class="products-body">
+        <div class="product">
+            <img src="img/card.PNG" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card1.PNG" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card2.PNG" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card3.PNG" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+    </div>
+    <div class="products-body">
+        <div class="product">
+            <img src="img/card4.png" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card5.png" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card6.png" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+        <div class="product">
+            <img src="img/card1.png" alt="">
+            <div class="img-description">
+                <h4>Apple Fruit</h4>
+                <p>$14.00-<del>$9.99</del></p>
+            </div>
+        </div>
+    </div>
+    
+</section>
+<br><br>
+
+
+
+
+
+
+<!--product section 2 end-->
+
+<!--offer start-->
+
+<section class="offer">
+
+    <div class="offer-container">
+
+        <div class="offer-item1">
+            <div class="middle-btn">
+                <a href="#" class="middle-btn-btn">Shop Now</a>
+            </div>
+        </div>
+        <div class="offer-item2">
+            <div class="middle-btn">
+                <a href="#" class="middle-btn-btn">Shop Now</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+<br><br>
+
+
+
+
+<!--offer end -->
+
+<!--Blog start-->
+
+
+  <section class="blog-section">
+    <h1>Our Blog</h1><br><br>
+    <div class="blog-container">
+        <div class="blog-item1"> 
+            <div class="middle-btn">
+                <a href="#" class="middle-btn-btn">Shop Now</a>
+            </div>
+        </div>
+        <div class="blog-item2">
+
+            <div class="blog-item2-1">
+                <div class="middle-btn">
+                    <a href="#" class="middle-btn-btn">Shop Now</a>
+                </div>
+            </div>
+            <div class="blog-item2-2">
+                <div class="middle-btn">
+                    <a href="#" class="middle-btn-btn">Shop Now</a>
+                </div>
+            </div>
+            <div class="blog-item2-3">
+                <div class="middle-btn">
+                    <a href="#" class="middle-btn-btn">Shop Now</a>
+                </div>
+            </div>
+            <div class="blog-item2-4">
+                <div class="middle-btn">
+                    <a href="#" class="middle-btn-btn">Shop Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+  
+  <!--Blog end-->
+
+</div>
+
+<!--product footer start-->
+
+<footer class="footer-distributed">
+
+    <div class="footer-left">
+  <img src="img/logo.png">
+        <h3>About<span>Eduonix</span></h3>
+
+        <p class="footer-links">
+            <a href="#">Home</a>
+            |
+            <a href="#">Blog</a>
+            |
+            <a href="#">About</a>
+            |
+            <a href="#">Contact</a>
+        </p>
+
+        <p class="footer-company-name">© 2022 E-commerce Fruits  Pvt. Ltd.</p>
+    </div>
+
+    <div class="footer-center">
+        <div>
+              <p><span>641 - Terokhadia ,
+                  Cantonment.
+              </span>
+                Rajpara,Rajshahi</p>
+        </div>
+
+        <div>
+           
+            <p>+880 1314441196</p>
+        </div>
+        <div>
+           
+            <p><a href="mailto:support@eduonix.com">shohanurrahman2334@gmail.com</a></p>
+        </div>
+    </div>
+    <div class="footer-right">
+        <p class="footer-company-about">
+            <span>About the E-commerce Fruit Product</span>
+            We offer fruits Product and update fruits.</p>
+        <div class="footer-icons">
+            <a href="#"><img src="img/visa.png"></a>
+            <a href="#"><img src="img/index.png"></a>
+            <a href="#"><img src="img/roket.png"></a>
+            <a href="#"><img src="img/nogod.png"></a>
+            <a href="#"><img src="img/roket.png"></a>
+            <a href=""><img src="img/paypal.png"></a>
+        </div>
+    </div>
+</footer>
+
+<!--product footer end-->
+
+
+
+</body>
+</html>
