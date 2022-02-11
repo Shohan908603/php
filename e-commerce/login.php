203b3b503a4df44566350687ea561cc34908483f
shohan <shohanurrahman2334@gmail.com>
2022-02-12 02:51:43 +0600

    login from add

---
+++ e-commerce/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce</title>
+</head>
+<body>
+    
+        <h2 style="text-align: center; margin-top: 50px;">Log In</h2>
+            <div class="main">
+                <div class="main-section">
+                    <div class="signin-section">
+                        <form class="from-section">
+                            <div style="display: flex;"> 
+                                <div style="font-size: 20px;"><b> Sign In </b></div>
+                                <div style="margin: 0 0 0 230px; word-spacing: 15px;">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                    <i class="fa-brands fa-twitter"></i>
+                                    <i class="fa-brands fa-google"></i>
+                                </div>
+                            </div><br>
+                            <b>USERNAME</b><br>
+                            <input type="text" placeholder="Username"><br><br>
+                            <b>PASSWORD</b><br>
+                            <input type="password" placeholder="Password"><br>
+                            <input type="submit" value="Sing In"><br><br>
+
+                            <div style="display: flex;">
+                                <div style="color: rgb(223, 109, 94);">
+                                    <input type="checkbox">Remember Me
+                                </div>
+                                <div style="margin: 0 0 0 150px; cursor: pointer; color: rgb(0, 0, 0);">
+                                    Forget Password
+                                </div>
+                            </div>
+
+
+                        </form>
+                    </div>
+                
+                    <div class="signup-section">
+                        <div class="signup-center">
+
+                            <div style="font-size: 34px; ">Welcome to login</div>
+                            <br><br>
+                            Don't have an account<br><br>
+                            
+                               <input type="submit" value="Sign Up">
+                            
+                        </div>    
+                    </div>
+                </div>
+            </div>    
+        
+    
+</body>
+</html>
