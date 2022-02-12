bda6bad97d1e5f5816c0dc3f3592e139e21affa0
shohan <shohanurrahman2334@gmail.com>
2022-02-12 14:12:17 +0600

    ragistration from add

---
+++ e-commerce/registration.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <link rel="stylesheet" href="css/registration.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>E-commerce</title>
+</head>
+<body>
+    <h1 style="text-align: center; margin-top: 40px;">Registration From</h1>
+    <div class="container">
+        
+        <div class="main">
+            
+            <div class="main-margin">
+                <form class="from-section"  >
+                    <h4>Sign Up</h4>
+
+                    
+                        <input type="text" class="text"  placeholder="First Name *" name="Frist Name">
+                        <input type="text" class="text" placeholder="Last Name *" name="Last Name"><br><br>
+                
+
+                <input type="password" class="text" placeholder="Password *" name="Password">
+                <input type="password" class="text" placeholder="Confirm Password *" name="Confirm Password"><br><br>
+                    <b>Gender:</b>
+                <input type="radio" name="Male" >Male
+                <input type="radio" name="Female">Female<br><br>
+                <input type="email" class="email" placeholder="Your Email *" name="Your Email"><br><br>
+                <input type="number" class="email" placeholder="Your Phone *" name="Your phone"><br><br>
+                <b>Comment</b><br>
+                <textarea class="comment" name="Comment"></textarea><br><br>
+                <input type="checkbox" name="checkbox"><b>Remember</b>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                
+                <input type="submit" class="register" name="Register" value="Register">
+                </form>
+            </div>
+
+            <div class="main-right">
+                <div class="icon">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-facebook-messenger"></i>
+                    <i class="fa-brands fa-google-plus-g"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                </div>
+               
+               <h3> <b>Welcome to Registration From</b></h3>
+
+                <b>Don't have an account</b><br><br><br>
+
+                <input type="submit" class="submit" value="Sign In">
+            </div>
+            
+        </div>
+       
+    </div>    
+
+   
+</body>
+</html>
