c32a0deb1ce0c46098bf7c7efa5860cc2a352351
shohan <shohanurrahman2334@gmail.com>
2022-02-15 20:31:09 +0600

    javascript from validate done

--- javascript/index.php
+++ javascript/index.php
@@ -1,18 +1,4 @@
-<?php
 
-
- echo "<pre>";
- print_r($_POST);
- echo "</pre>";
- 
-
-
-
-
-
-
-
-?>
 
 
 
@@ -29,7 +15,86 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 
-<script type="text/javascript">
+
+
+
+
+<body>
+    <div style="padding-top: 3%;">
+        <form class="col-md-4 border shadow-sm bg-light"
+        onsubmit="return matchpass()" name="f1"
+         style="margin-top:8%;padding:10px; width: 50%; margin: auto;padding-top: 2%;" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);  ?>" method="post">
+            <div>
+                <h2>Sign Up</h2>
+            </div>
+            <div style="display: flex;" >
+                <div style="padding-left:40px ;">
+                    <label>Frist Name </label><br>
+                    <input type="text" id="fname" placeholder="Your Frist name" autocomplete="off" name="fname">
+                </div>
+
+                <div style="padding-left: 5px;">
+                    <label>Last Name </label><br>
+                    <input type="text" id="lname" placeholder="Your Last name" autocomplete="off" name="lname">
+                </div>
+            </div>
+
+            <div style="padding-left:40px ; padding-top: 5%;">
+                <label>Gender </label>
+                <input type="radio" id="gender"  name="gender">Male
+                <input type="radio" id="gender"  name="gender">Female
+            </div>
+
+            <div style="display: flex; padding-top: 5%;" >
+                <div style="padding-left:40px ; ">
+                    <label>Mobile </label><br>
+                    <input type="number" id="mobile" placeholder="Your Phone Number" autocomplete="off" name="mobile">
+                </div>
+
+                <div style="padding-left: 5px;">
+                    <label>E-mail Name </label><br>
+                    <input type="email" id="email" placeholder="Your E-mail name" autocomplete="off" name="email">
+                    <span id="emailmsg"></span>
+                </div>
+            </div>
+
+            <div style="padding-left: 40px; width: 93%; padding-top: 5%;" >
+                <label>Source of funds</label><br>
+               <select class="btn btn-#FFFFFF btn-block" name="select">
+                   <option>Others</option>
+                   <option>Student</option>
+                   <option>Teacher</option>
+                   <option>Engineer</option>
+                   <option>Doctor</option>
+                   <option>Businessman</option>
+               </select>
+            </div>
+
+            <div style="display: flex; padding-top: 5%;" >
+                <div style="padding-left: 40px; " >
+                    <label> Password </label><br>
+                    <input type="password" id="password" placeholder="Your Password " autocomplete="off" name="password" ><br>
+                    
+                </div>
+
+                <div style="padding-left: 5px;">
+                    <label>Confirm Password </label><br>
+                    <input type="password" id="cpassword" placeholder="Your Confirm Password" autocomplete="off" name="cpassword"><br>
+                </div>
+            </div>
+
+            <div style="padding-left: 40px; padding-top: 5%;">
+                <input type="checkbox" id="checkbox" name="checkbox">&nbsp;&nbsp; I accept the <span style="color: #4106c9;cursor: pointer;">Terms and Conditions</span><br>
+                <input type="checkbox" id="checkbox" name="checkbox">&nbsp;&nbsp; I accept the <span style="color: #4106c9; cursor: pointer;">Code of Conduct & Ethics</span>
+            </div>
+
+            <div style="padding-left: 40px;width: 93%; padding-top: 5%;">
+                <input type="submit"  class="btn btn-success btn-block" id="button" name="button" value="Button" >
+            </div>
+        </form>
+    </div>
+
+    <script type="text/javascript">
 	$(document).ready(function(){
 		
 		$("#fname").keyup(function(){
@@ -79,6 +144,16 @@
             buttonSate();
         });
 
+        $("#password").keyup(function(){
+            if (validatepassword()){
+                $("#password").css("border","2px solid green");
+            }
+            else{
+                $("#password").css("border","2px solid red");
+            }
+            buttonSate();
+        });
+
         $("#cpassword").keyup(function(){
             if (validatecpassword()){
                 $("#cpassword").css("border","2px solid green");
@@ -88,14 +163,27 @@
             }
             buttonSate();
         });
-
-       
-
-
-
 		
 	});
 
+
+
+
+    function matchpass(){
+        var firstpassword=document.f1.password.value;
+        var secondpassword=document.f1.cpassword.value;
+
+        if(firstpassword==secondpassword){
+            alert("success");
+            return true;
+
+        }
+
+        else{
+            alert("password are not match \n Enter the correct password");
+            return false;
+        }
+    }
 	
 	function validatefname(){
 		
@@ -141,6 +229,18 @@
 
         var reg =/^\d{11}$/;
         if(reg.test(mobile)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function validatepassword(){
+        var password=$("#password").val();
+
+        var reg =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+        if(reg.test(password)){
             return true;
         }
         else{
@@ -230,78 +330,6 @@
 
 
 
-<body>
-    <div style="padding-top: 3%;">
-        <form class="col-md-4 border shadow-sm bg-light"
-         style="margin-top:8%;padding:10px; width: 50%; margin: auto;padding-top: 2%;" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);  ?>" method="post">
-            <div>
-                <h2>Sign Up</h2>
-            </div>
-            <div style="display: flex;" >
-                <div style="padding-left:40px ;">
-                    <label>Frist Name </label><br>
-                    <input type="text" id="fname" placeholder="Your Frist name" autocomplete="off" name="fname">
-                </div>
 
-                <div style="padding-left: 5px;">
-                    <label>Last Name </label><br>
-                    <input type="text" id="lname" placeholder="Your Last name" autocomplete="off" name="lname">
-                </div>
-            </div>
-
-            <div style="padding-left:40px ; padding-top: 5%;">
-                <label>Gender </label>
-                <input type="radio" id="gender"  name="gender">Male
-                <input type="radio" id="gender"  name="gender">Female
-            </div>
-
-            <div style="display: flex; padding-top: 5%;" >
-                <div style="padding-left:40px ; ">
-                    <label>Mobile </label><br>
-                    <input type="number" id="mobile" placeholder="Your Phone Number" autocomplete="off" name="mobile">
-                </div>
-
-                <div style="padding-left: 5px;">
-                    <label>E-mail Name </label><br>
-                    <input type="email" id="email" placeholder="Your E-mail name" autocomplete="off" name="email">
-                    <span id="emailmsg"></span>
-                </div>
-            </div>
-
-            <div style="padding-left: 40px; width: 93%; padding-top: 5%;" >
-                <label>Source of funds</label><br>
-               <select class="btn btn-#FFFFFF btn-block" name="select">
-                   <option>Others</option>
-                   <option>Student</option>
-                   <option>Teacher</option>
-                   <option>Engineer</option>
-                   <option>Doctor</option>
-                   <option>Businessman</option>
-               </select>
-            </div>
-
-            <div style="display: flex; padding-top: 5%;" >
-                <div style="padding-left: 40px; " >
-                    <label> Password </label><br>
-                    <input type="text" id="password" placeholder="Your Password " autocomplete="off" name="password" ><br>
-                    <progress max="100" value="0" id="meter"></progress>
-                </div>
-
-                <div style="padding-left: 5px;">
-                    <label>Confirm Password </label><br>
-                    <input type="password" id="cpassword" placeholder="Your Confirm Password" autocomplete="off" name="cpassword"><br>
-                </div>
-            </div>
-
-            <div style="padding-left: 40px; padding-top: 5%;">
-                <input type="checkbox" id="checkbox" name="checkbox">&nbsp;&nbsp; I accept the <span style="color: #4106c9;cursor: pointer;">Terms and Conditions</span><br>
-                <input type="checkbox" id="checkbox" name="checkbox">&nbsp;&nbsp; I accept the <span style="color: #4106c9; cursor: pointer;">Code of Conduct & Ethics</span>
-            </div>
-
-            <div style="padding-left: 40px;width: 93%; padding-top: 5%;">
-                <input type="submit"  class="btn btn-success btn-block" id="button" name="button" value="Button" >
-            </div>
-        </form>
-    </div>
 </body>
 </html>
